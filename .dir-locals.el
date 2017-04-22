cb7221fae91244f21e9610d22ca4b6f0cd6365a1
Fabián Ezequiel Gallina <galli.87@gmail.com>
2017-04-22 09:46:20 -0300

    Fix .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,4 @@
   (tab-width . 4)
   (fill-column . 78))
  (org-mode
-  (org-export-babel-evaluate . nil))
- (python-mode
-  (python-shell-virtualenv-path . "/home/fgallina/Code/Projects/ConociendoPython/code/env3")))
+  (org-export-babel-evaluate . nil)))
